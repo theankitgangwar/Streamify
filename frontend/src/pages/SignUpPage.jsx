51383da379fe51084b03117c5299e51415d73a30
theankitgangwar <theankitgangwar@gmail.com>
2025-10-06 21:43:43 +0530

    Initialising-project

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -49,7 +49,7 @@ const SignUpPage = () => {
           {/* ERROR MESSAGE IF ANY */}
           {error && (
             <div className="alert alert-error mb-4">
-              <span>{error.response.data.message}</span>
+              <span>{error?.response?.data?.message}</span>
             </div>
           )}
 
